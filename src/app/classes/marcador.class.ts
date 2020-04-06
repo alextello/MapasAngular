8987f0bd710279aa8c0f47776ec50e20d15daa8e
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-04-06 11:49:43 -0600

    Se añaden marcadores al mapa

---
+++ src/app/classes/marcador.class.ts
@@ -0,0 +1,6 @@
+export class Marcador {
+    public titulo = 'Sin titulo';
+    public descripcion = 'Sin descripción';
+    constructor(public lat: number, public lng: number) {
+    }
+}
--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from '../../classes/marcador.class';
 
 @Component({
   selector: 'app-mapa',
@@ -7,12 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  marcadores: Marcador[] = [];
   lat = 51.54954;
   lng = 7.802484;
   constructor() {
+    const nuevoMarcador = new Marcador(51.54954, 7.802484);
+    this.marcadores.push(nuevoMarcador);
   }
 
   ngOnInit(): void {
+  }
+  agregarMarcador(evento) {
+    const coords: {lat: number, lng: number} = evento.coords;
+    const nuevoMarcador = new Marcador(coords.lat, coords.lng);
+    this.marcadores.push(nuevoMarcador);
   }
 
 }
